a7b22eef5ed7963eb134e7cd1c23755ee6026f5e
kobakobashu <kobaya4@fuji.waseda.jp>
2023-04-27 12:25:28 +0900

    add multi chars local variable

--- 9cc/parse.c
+++ 9cc/parse.c
@@ -73,7 +73,12 @@ Token *tokenize() {
     }
 
     if ('a' <= *p && *p <= 'z') {
-      cur = new_token(TK_IDENT, cur, p++, 1);
+      cur = new_token(TK_IDENT, cur, p, 0);
+      char *q = p;
+      while ('a' <= *p && *p <= 'z' || *p == '_' || isdigit(*p)) {
+        p++;
+      }
+      cur->len = p - q;
       continue;
     }
 
@@ -87,6 +92,17 @@ Token *tokenize() {
 //
 // parser
 //
+
+typedef struct LVar LVar;
+
+struct LVar {
+  LVar *next;
+  char *name;
+  int len;
+  int offset;
+};
+
+LVar *locals;
 
 static Node *expr();
 
@@ -145,6 +161,15 @@ static Node *new_node_num(int val) {
   return node;
 }
 
+LVar *find_lvar(Token *tok) {
+  for (LVar *var = locals; var; var = var->next) {
+    if (var->len == tok->len && !memcmp(var->name, tok->str, var->len)) {
+      return var;
+    }
+  }
+  return NULL;
+}
+
 static Node *primary() {
   if (consume("(")) {
     Node *node = expr();
@@ -155,7 +180,24 @@ static Node *primary() {
   if (tok) {
     Node *node = calloc(1, sizeof(Node));
     node->kind = ND_LVAR;
-    node->offset = (tok->str[0] - 'a' + 1) * 8;
+
+    LVar *lvar = find_lvar(tok);
+    if (lvar) {
+      node->offset = lvar->offset;
+    } else {
+      lvar = calloc(1, sizeof(LVar));
+      lvar->next = locals;
+      lvar->name = tok->str;
+      lvar->len = tok->len;
+      if (locals) {
+        lvar->offset = locals->offset + 8;
+      } else {
+        lvar->offset = 0;
+      }
+      node->offset = lvar->offset;
+      locals = lvar;
+    }
+    // node->offset = (tok->str[0] - 'a' + 1) * 8;
     return node;
   }
   return new_node_num(expect_number());
