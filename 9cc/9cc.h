7079585a5507d52613bec9398c4fb399775bd4ad
kobakobashu <kobaya4@fuji.waseda.jp>
2023-11-16 18:17:20 +1100

    add void type

--- 9cc/9cc.h
+++ 9cc/9cc.h
@@ -139,6 +139,7 @@ void codegen(Obj *prog, FILE *out);
 //
 
 typedef enum {
+  TY_VOID,
   TY_CHAR,
   TY_SHORT,
   TY_INT,
@@ -181,6 +182,7 @@ struct Member {
   int offset;
 };
 
+extern Type *ty_void;
 extern Type *ty_int;
 extern Type *ty_char;
 extern Type *ty_long;
